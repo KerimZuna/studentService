9d83b21890ffa97d0988dee86741113aa1240c77
nazezu <kerimzuna18@gmail.com>
2023-03-30 15:09:23 +0200

    Adding existing project files to the repo

---
+++ ProjekatPraksa/Controllers/StudentController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjekatPraksa.Data;
+using ProjekatPraksa.Models;
+
+namespace ProjekatPraksa.Controllers
+{
+    [Authorize]
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Student
+        public async Task<IActionResult> Index()
+        {
+              return _context.Students != null ? 
+                          View(await _context.Students.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Students'  is null.");
+        }
+
+        // GET: Student/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Students == null)
+            {
+                return NotFound();
+            }
+
+            var studentEntity = await _context.Students
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (studentEntity == null)
+            {
+                return NotFound();
+            }
+
+            return View(studentEntity);
+        }
+
+        // GET: Student/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Mobile,Email,Source")] StudentEntity studentEntity)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(studentEntity);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(studentEntity);
+        }
+
+        // GET: Student/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Students == null)
+            {
+                return NotFound();
+            }
+
+            var studentEntity = await _context.Students.FindAsync(id);
+            if (studentEntity == null)
+            {
+                return NotFound();
+            }
+            return View(studentEntity);
+        }
+
+        // POST: Student/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Mobile,Email,Source")] StudentEntity studentEntity)
+        {
+            if (id != studentEntity.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(studentEntity);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StudentEntityExists(studentEntity.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(studentEntity);
+        }
+
+        // GET: Student/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Students == null)
+            {
+                return NotFound();
+            }
+
+            var studentEntity = await _context.Students
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (studentEntity == null)
+            {
+                return NotFound();
+            }
+
+            return View(studentEntity);
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Students == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Students'  is null.");
+            }
+            var studentEntity = await _context.Students.FindAsync(id);
+            if (studentEntity != null)
+            {
+                _context.Students.Remove(studentEntity);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StudentEntityExists(int id)
+        {
+          return (_context.Students?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ ProjekatPraksa/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProjekatPraksa.Models;
+
+namespace ProjekatPraksa.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<StudentEntity> Students { get; set; }
+    }
+}
